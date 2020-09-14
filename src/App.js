aaa91dcce240c79d468a937c34bb5455289c2eb9
Disa A. Syuhada <disaada@gmail.com>
2020-09-14 18:01:46 +0700

    add registration notice

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 
 import Base from './components/Base'
-import Registration from './components/Registration'
 
 function App() {
   return (
     <div className="App">
       <Base />
-      <Registration />
     </div>
   );
 }
--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import RegistrationNotice from './RegistrationNotice'
+import Registration from './Registration'
 
 function Base () {
     return (
         <div>
             <BrowserRouter>
                 <Switch>
+                    <Route exact path="/" component={Registration} />
                     <Route path="/RegistrationNotice" component={RegistrationNotice} />
                 </Switch>
             </BrowserRouter>
